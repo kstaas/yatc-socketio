1d15ef75a7209c6a74ec3303dc8da3e377f88ccd
Karl Staas <karl@staas.org>
2020-04-11 19:39:28 -0400

    Chat message signagure is (color,name,message). Server chooses color.

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+// server selects color.
+var cindex = 0;
+var colors = [ 'red', 'blue', 'green', 'orange', 'cyan', 'pink', 'purple' ];
+
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
@@ -11,12 +15,14 @@ io.on('connection', function(socket) {
   socket.on('chat message', function(name, msg) {
     if (socket.name == undefined) {
       socket.name = name;
+      socket.color = colors[cindex];
+      cindex = (cindex + 1) % colors.length;
     }
     if (socket.name != name) {
-      io.emit('chat message', `${socket.name}: changed name to ${name}`)
+      io.emit('chat message', socket.color, socket.name, `changed name to ${name}`)
       socket.name = name;
     }
-    io.emit('chat message', `${name}: ${msg}`);
+    io.emit('chat message', socket.color, socket.name, msg);
   });
 });
 
