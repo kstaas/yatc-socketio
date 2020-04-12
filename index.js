8fe92b81ffa37e17ccc89ad2b090dacc924f46b1
Karl Staas <karl@staas.org>
2020-04-12 10:40:09 -0400

    Ignore anything that has no name. Do not emit zero length messages.

--- index.js
+++ index.js
@@ -13,16 +13,24 @@ app.get('/', function(req, res) {
 
 io.on('connection', function(socket) {
   socket.on('chat message', function(name, msg) {
-    if (socket.name == undefined) {
-      socket.name = name;
-      socket.color = colors[cindex];
-      cindex = (cindex + 1) % colors.length;
+    // Unless there is a name, do nothing!
+    if (name.length) {
+      // If there's no name associated with this socket yet, then associate this name.
+      if (socket.name == undefined) {
+        socket.name = name;
+        socket.color = colors[cindex];
+        cindex = (cindex + 1) % colors.length;
+      }
+      // If this name is different than the name that associated with this socket then announce that fact.
+      if (socket.name != name) {
+        io.emit('chat message', 'black', socket.name, `changed name to ${name}`)
+        socket.name = name;
+      }
+      // Only emit messages, not blanks.
+      if (msg.length) {
+        io.emit('chat message', socket.color, socket.name, msg);
+      }
     }
-    if (socket.name != name) {
-      io.emit('chat message', socket.color, socket.name, `changed name to ${name}`)
-      socket.name = name;
-    }
-    io.emit('chat message', socket.color, socket.name, msg);
   });
 });
 
