46619373b33fd3a13b3894b5ce7fde4817f8269c
Karl Staas <karl@staas.org>
2020-04-25 16:14:00 -0400

    updates

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+/* vim:set ts=2 sw=2 expandtab: */
+
+//
+// server
+//
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+app.use(express.static('public'));
+
+http.listen(port, function() {
+  console.log('listening on *:' + port);
+});
+
+//
+// chat
+//
+
 // server selects color.
 var cindex = 0;
 var colors = [ 'red', 'blue', 'green', 'orange', 'cyan', 'pink', 'purple' ];
-
-app.use(express.static('public'));
 
 io.on('connection', function(socket) {
   socket.on('chat message', function(name, msg) {
@@ -33,6 +46,3 @@ io.on('connection', function(socket) {
   });
 });
 
-http.listen(port, function() {
-  console.log('listening on *:' + port);
-});
